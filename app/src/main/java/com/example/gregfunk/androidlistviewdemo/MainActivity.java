f701cd5e304a20a21aa01a7ef7aeeb612d35783c
Greg Funk <greg.funk@walltowall.com>
2015-11-16 13:47:58 -0500

    Countdown timer

--- app/src/main/java/com/example/gregfunk/androidlistviewdemo/MainActivity.java
+++ app/src/main/java/com/example/gregfunk/androidlistviewdemo/MainActivity.java
@@ -47,12 +47,21 @@ public class MainActivity extends AppCompatActivity {
         Runnable run = new Runnable() {
             @Override
             public void run() {
-                Log.i("Runnable has run", "a second must have passed");
+                //Log.i("Runnable has run", "a second must have passed");
 
                 handler.postDelayed(this, 1000);
             }
         };
         handler.post(run);
-        
+
+
+        new CountDownTimer(10000, 1000) {
+            public void onTick(long millisecondsUntilDone) {
+                Log.i("seconds left", String.valueOf(millisecondsUntilDone / 1000));
+            }
+            public void onFinish() {
+                Log.i("Done!",  "Countdown timer finished");
+            }
+        }.start();
     }
 }
