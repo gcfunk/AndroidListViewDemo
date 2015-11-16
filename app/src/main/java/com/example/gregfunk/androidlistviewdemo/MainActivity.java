d0d7f6cd2aaba34e66b02c7e551d55d9895e5141
Greg Funk <greg.funk@walltowall.com>
2015-11-16 11:34:50 -0500

    Adds list view to screen

--- app/src/main/java/com/example/gregfunk/androidlistviewdemo/MainActivity.java
+++ app/src/main/java/com/example/gregfunk/androidlistviewdemo/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.gregfunk.androidlistviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+
+        ArrayList<String> family = new ArrayList<String>();
+        family.add("Greg");
+        family.add("Niki");
+        family.add("Riley");
+        family.add("Jesse");
+        family.add("Joey");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, family);
+        myListView.setAdapter(arrayAdapter);
     }
 }
